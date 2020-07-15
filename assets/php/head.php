57e784d0ea44d8665b3f125274f4eb3cc0086173
Lauren Kapraun <laurenkapraun@gmail.com>
2020-07-15 17:51:37 -0500

    Assessment Upload

---
+++ assets/php/head.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>Lauren Kapraun | HLM Skills Assessment</title>
+    
+    <!-- Compressed CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+    
+    <!-- foundation-float.min.css: Compressed CSS with legacy Float Grid -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation-float.min.css" integrity="sha256-4ldVyEvC86/kae2IBWw+eJrTiwNEbUUTmN0zkP4luL4=" crossorigin="anonymous">
+    
+    <!-- foundation-prototype.min.css: Compressed CSS with prototyping classes -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation-prototype.min.css" integrity="sha256-BiKflOunI0SIxlTOOUCQ0HgwXrRrRwBkIYppEllPIok=" crossorigin="anonymous">
+    
+    <!-- foundation-rtl.min.css: Compressed CSS with right-to-left reading direction -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation-rtl.min.css" integrity="sha256-F+9Ns8Z/1ZISonBbojH743hsmF3x3AlQdJEeD8DhQsE=" crossorigin="anonymous">
+
+    <!-- Compressed JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    
+    <!-- Lato & Monsterrat Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Montserrat:wght@500;700;800&display=swap" rel="stylesheet">
+</head>
---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <?php include("assets/php/head.php"); ?>
+    <body>
+        <header>
+            <div class="grid-container header">
+                <div class="grid-x grid-padding-x align-middle">
+                    <div class="large-4 cell">
+                        <img src="assets/images/logo.png" alt="Cedar Works Deck Builders Logo" class="header-logo">
+                        <h1 class="visually-hidden">Cedar Works Deck Builders</h1>
+                    </div>
+                    <div class="medium-6 large-3 cell">
+                        <p class="header-tagline"><em>Creating Southeast Michigan’s Dream Outdoor Spaces Since 1990</em></p>
+                    </div>
+                    <div class="medium-6 large-4 cell">
+                        <p>Start Your Outdoor Project Today!</p>
+                        <p class="header-phone_number">(248) 363-1113</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <div class="banner_hero_section" role="img" aria-label="Image of Custom Made Deck">
+                <div class="banner_hero_section-text">
+                    <h2 class="banner_hero_section-text-header">Relax and Enjoy Summer with a <span class="banner_hero_section-text-header-cta"><img src="assets/images/banner-underline.png" alt="Blue Banner Underline">Custom-Made</span> Deck or Patio</h2>
+                    <p class="banner_hero_section-text-paragraph">There’s no better way to spend the warm weather months with friends and family than on a custom designed deck or under the pergola or gazebo you’ve always wanted</p>
+                    <hr>
+                    <a href="#" class="button">View Our Portfolio</a>
+                    <br>
+                    <img src="assets/images/banner_trex_pro_logo.png" alt="TrexPRO Platinum Logo" class="banner_hero_section-text-trex_logo">
+                </div>
+            </div>
+        </section>
+        
+        <section>
+            <div class="testimonials" role="img" aria-label="Image of Wood for Background">
+                <h2>Our Passion is Creating a Comfortable and Functional Outdoor Living Space for You</h2>
+                <hr>
+                <div class="grid-container">
+                    <div class="grid-x grid-padding-x align-top">
+                        <div class="medium-6 cell">
+                            <p class="testimonials_title">“We had such a POSITIVE experience!”</p>
+                            <p>“A crew of five came out and tore out my old deck in no time at all. The cleanup was impeccable. The same morning the lumber was delivered so Pat and Mike could start building our new cedar deck that afternoon. Pat has been with Cedar Works over 20 years and he and Mike did such quality work during their few days here. They were hard working, genuinely nice guys. All my neighbors and friends are pleasantly surprised what a fantastic job they did. We are two very happy customers! We recommend them highly - a 10 plus in our book!”</p>
+                            <div class="testimonials_stars">
+                                <img src="assets/images/star.png" alt="Review #1 Star 1">
+                                <img src="assets/images/star.png" alt="Review #1 Star 2">
+                                <img src="assets/images/star.png" alt="Review #1 Star 3">
+                                <img src="assets/images/star.png" alt="Review #1 Star 4">
+                                <img src="assets/images/star.png" alt="Review #1 Star 5">
+                            </div>
+                            <img src="assets/images/google_logo.png" alt="Google Logo for Review #1">
+                        </div>
+                        <div class="medium-6 cell">
+                            <p class="testimonials_title">“Everything was handled well.”</p>
+                            <p>“My wife and I would highly recommend Cedar Works if you are considering a deck for your home. They installed a Trex deck for us and did a great job with the project. Conversations with the owner of Cedar Works and his advice really helped with our design considerations and led to a design we are very happy with. The Cedar Works crew was very efficient and professional. Everything from managing the building materials to keeping our yard clear of debris was handled well. It’s great to work with people who know what they are doing.”</p>
+                            <div class="testimonials_stars">
+                                <img src="assets/images/star.png" alt="Review #1 Star 1">
+                                <img src="assets/images/star.png" alt="Review #1 Star 2">
+                                <img src="assets/images/star.png" alt="Review #1 Star 3">
+                                <img src="assets/images/star.png" alt="Review #1 Star 4">
+                                <img src="assets/images/star.png" alt="Review #1 Star 5">
+                            </div>
+                            <img src="assets/images/google_logo.png" alt="Google Logo for Review #1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="lead_form_section" role="img" aria-label="Image of Buildings for Background">
+                <div class="grid-container">
+                    <div class="grid-x grid-padding-x align-middle">
+                        <div class="large-6 cell lead_form_section-left">
+                            <h2>Our Deck and Outdoor Living Experts Strive to Exceed Your Expectations</h2>
+                            <p>Throughout the entire process, we are here to provide you with the highest levels of quality, reliability, attention to detail, and professionalism.</p>
+                            <div class="grid-container">
+                                <div class="grid-x grid-padding-x align-top">
+                                    <div class="medium-6 cell">
+                                        <ul>
+                                            <li>Over 20 Years of Experience</li>
+                                            <li>Work Year Round</li>
+                                            <li>Owner Operated</li>
+                                        </ul>
+                                    </div>
+                                    <div class="medium-6 cell">
+                                        <ul>
+                                            <li>5-Year Guarantee</li>
+                                            <li>TrexPro&reg; Platinum Contractors</li>
+                                            <li>Fully Licensed and Insured</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <p><strong>We’ll build you the deck of your dreams!</strong></p>
+                        </div>
+                        <div class="large-6 cell lead_form_section-right">
+                            <div class="lead_form_section-right-outer">
+                                <div class="lead_form_section-right-inner">
+                                    <h3>Ready to Start Your Outdoor Project?</h3>
+                                    <form>
+                                        <p class="lead_form_section-right-inner-req">* Indicates Required Questions</p>
+                                        <div class="grid-container">
+                                            <div class="grid-x grid-padding-x">
+                                                <div class="medium-6 cell">
+                                                    <label for="FirstName" class="visually-hidden">First Name</label>
+                                                    <input type="text" placeholder="First Name*" id="FirstName" required>
+                                                </div>
+                                                <div class="medium-6 cell">
+                                                    <label for="LastName" class="visually-hidden">Last Name</label>
+                                                    <input type="text" placeholder="Last Name*" id="LastName" required>
+                                                </div>
+                                                <div class="small-12 cell">
+                                                    <label for="eMail" class="visually-hidden">eMail</label>
+                                                    <input type="email" placeholder="Email*" id="eMail" required>
+                                                </div>
+                                                <div class="small-12 cell">
+                                                    <label for="PhoneNumber" class="visually-hidden">Phone Number</label>
+                                                    <input type="tel" placeholder="Phone Number*" id="PhoneNumber" required>
+                                                </div>
+                                                <div class="small-12 cell">
+                                                    <label for="Address" class="visually-hidden">Address</label>
+                                                    <textarea placeholder="Address*" id="Address" rows="4" required></textarea>
+                                                </div>
+                                                <div class="small-12 cell">
+                                                    <label for="Project" class="visually-hidden">Tell us about your project</label>
+                                                    <textarea placeholder="Tell us about your project*" id="Project" rows="4" required></textarea>
+                                                </div>
+                                                <div class="small-12 cell input-group-button">
+                                                    <input type="submit" class="button submit" value="Get Started">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section>
+            <div class="footer_callout_section">
+                <h2 class="footer_callout_section-text">Contact Cedar Works Today to Get Started on Your Next Deck or Outdoor Living Project!</h2>
+                <p class="footer_callout_section-phone">(248) 365-7738</p>
+            </div>
+        </section>
+        
+        <footer>
+            <p>The contents of this webpage are Copyright &copy; 2020 Cedar Works Deck Builders. All Rights Reserved.</p>
+            
+            <div class="footer_links">
+                <p><a href="#">Web Design</a></p>
+                <p><a href="#">Manage</a></p>
+                <p><a href="#">Privacy</a></p>
+            </div>
+        </footer>
+    </body>
+</html>
